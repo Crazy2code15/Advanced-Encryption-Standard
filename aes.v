7d8bcd5529600784091081b3757b1e78b08d3c1b
Pratibha Singh <63441963+Crazy2code15@users.noreply.github.com>
2022-02-09 17:09:52 +0530

    main

---
+++ aes.v
@@ -0,0 +1,18 @@
+// AES USING VERILOG
+
+module AES(key,inputText,enable,operation,outputText);
+    input [15:0] key;
+    input [15:0] inputText;
+    input enable;      
+    input operation;
+    output [15:0] outputText;
+	
+    wire [15:0] encryptedText,decryptedText,result;
+
+    Encryption e(key,inputText,encryptedText);
+    Decryption d(key,inputText,decryptedText);
+	
+    MUX_16Bit_2x1 mux1(decryptedText,encryptedText,operation,result);
+    MUX_16Bit_2x1 mux2(result,inputText,enable,outputText);
+    
+endmodule
